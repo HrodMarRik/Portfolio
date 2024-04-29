e79afb599375a6b4ec5e1685813cce840392f7d4
HrodMarRik <somosminal@gmail.com>
2024-04-29 12:08:34 +0200

    auto-up

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>RChanavat</title>
+</head>
+<body>
+
+<h1>titre pour titrer</h1>
+
+</body>
+</html>
