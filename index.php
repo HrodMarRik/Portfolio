262fcad5de48a1e28214a144e72a80da7200208e
HrodMarRik <somosminal@gmail.com>
2024-04-29 18:32:27 +0200

    auto-up

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// header
+require 'template/header.php';
+
+// acceuil
+require 'template/acceuil.php';
+
+// footer
+require 'template/footer.php';
+
+?>
--- template/header.php
+++ template/header.php
@@ -1,34 +1,65 @@
-  <header class="header-area header-sticky">
-      <div class="container">
-          <div class="row">
-              <div class="col-12">
-                  <nav class="main-nav">
-                      <!-- ***** Logo Start ***** -->
-                      <a href="index.html" class="logo">
-                          Edu Meeting
-                      </a>
-                      <!-- ***** Logo End ***** -->
-                      <!-- ***** Menu Start ***** -->
-                      <ul class="nav">
-                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                          <li><a href="meetings.html">Meetings</a></li>
-                          <li class="scroll-to-section"><a href="#apply">Apply Now</a></li>
-                          <li class="has-sub">
-                              <a href="javascript:void(0)">Pages</a>
-                              <ul class="sub-menu">
-                                  <li><a href="meetings.html">Upcoming Meetings</a></li>
-                                  <li><a href="meeting-details.html">Meeting Details</a></li>
-                              </ul>
-                          </li>
-                          <li class="scroll-to-section"><a href="#courses">Courses</a></li> 
-                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
-                      </ul>        
-                      <a class='menu-trigger'>
-                          <span>Menu</span>
-                      </a>
-                      <!-- ***** Menu End ***** -->
-                  </nav>
-              </div>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <meta name="description" content="Portfolio de Romaric Chanavat">
+  <meta name="author" content="Romaric Chanavat">
+  <meta name="copyright" content="Romaric Chanavat"/>
+
+  <title>R.Chanavat</title>
+
+  <link href="/Portfolio/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/Portfolio/assets/css/fontawesome.css">
+  <link rel="stylesheet" href="/Portfolio/assets/css/templatemo-edu-meeting.css">
+  <link rel="stylesheet" href="/Portfolio/assets/css/owl.css">
+  <link rel="stylesheet" href="/Portfolio/assets/css/lightbox.css">
+
+</head>
+
+
+<header class="header-area header-sticky">
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <nav class="main-nav">
+                  <a href="/Portfolio/index.php" class="logo">R.Chanavat</a>
+                  <ul class="nav">
+
+                      <li class="scroll-to-section"><a href="/Portfolio/index.php" class="active">Acceuil</a></li>
+
+                      <li><a href="/Portfolio/meetings.html">Entreprise</a></li>
+
+                      <li class="scroll-to-section"><a href="#apply">Veille</a></li>
+
+                      <li class="has-sub">
+                          <a href="javascript:void(0)">Travaux</a>
+                          <ul class="sub-menu">
+                              <li><a href="/Portfolio/meetings.html">Application bar</a></li>
+                              <li><a href="/Portfolio/meeting-details.html">Portfolio</a></li>
+                          </ul>
+                      </li>
+
+                      <li class="has-sub">
+                          <a href="javascript:void(0)">Projet</a>
+                          <ul class="sub-menu">
+                              <li><a href="/Portfolio/meetings.html">Projet 1</a></li>
+                              <li><a href="/Portfolio/meeting-details.html">Projet 2</a></li>
+                          </ul>
+                      </li>
+
+                      <li class="scroll-to-section"><a href="#contact">Compétence</a></li>
+
+                      <li><a href="#####">Répertoire</a></li>
+
+                  </ul>
+
+                  <a class='menu-trigger'><span>Menu</span></a>
+              </nav>
           </div>
       </div>
-  </header>
+  </div>
+</header>
+
