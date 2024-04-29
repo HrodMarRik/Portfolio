523fa863e42e3fc41b628d12f4c01424785bc059
HrodMarRik <somosminal@gmail.com>
2024-04-29 12:18:07 +0200

    auto-up

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>RChanavat</title>
-</head>
-<body>
-
-<h1>titre pour titrer</h1>
-
-</body>
-</html>
+<?php
+$template = "template/header.php";
+header("Location: " . $template);
+exit;
+?>
